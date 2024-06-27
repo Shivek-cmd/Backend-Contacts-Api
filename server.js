787cc2cccdc95ccc9633784f4f08ca0bb63e7561
Shivek-cmd <59970948+Shivek-cmd@users.noreply.github.com>
2024-06-28 02:56:08 +0530

    Set up MongoDB connection and implement CRUD operations for contacts

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler.js");
+const connectDb = require("./config/dbConnection.js");
 const app = express();
 const dotenv = require("dotenv").config();
+connectDb();
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes.js"));
