f9a9265827e8c29ae9327a92c9b1e2a0b9b4fd32
Shivek-cmd <59970948+Shivek-cmd@users.noreply.github.com>
2024-06-28 02:01:03 +0530

    Add error handling middleware and set up contact API routes

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,31 +1,37 @@
 //@desc get all contacts
 //@route GET /api/contacts
-//@acces public
+//@access public
 const getContacts = (req, res) => {
   res.status(200).json({ message: "Get all contacts" });
 };
 
 //@desc Create new Contact
 //@route POST /api/contacts
-//@acces public
+//@access public
 const createContact = (req, res) => {
+  console.log("The request body is", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
   res.status(201).json({ message: "Create Contact" });
 };
 //@desc Get Contact
 //@route GET /api/contacts/:id
-//@acces public
+//@access public
 const getContact = (req, res) => {
   res.status(200).json({ message: `Get Contact for ${req.params.id}` });
 };
 //@desc Update Contact
 //@route PUT /api/contacts/:id
-//@acces public
+//@access public
 const updateContact = (req, res) => {
   res.status(200).json({ message: `Update Contact for ${req.params.id}` });
 };
 //@desc Delete Contact
 //@route DELETE /api/contacts/:id
-//@acces public
+//@access public
 const deleteContact = (req, res) => {
   res.status(200).json({ message: `Delete Contact for ${req.params.id}` });
 };
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler.js");
 const app = express();
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
+app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes.js"));
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`server running on ${port}`);
